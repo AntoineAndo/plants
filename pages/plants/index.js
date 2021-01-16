cce2ce72962fafe54051c4f5d9eb08bae3d18084
Antoine <antoine.ando@gmail.com>
2021-01-16 23:16:53 +0100

    GET query ok

--- pages/plants/index.js
+++ pages/plants/index.js
@@ -2,13 +2,43 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../../components/layout'
 
+import axios from 'axios'
 
-export default function Index() {
+export default function Index({plants}) {
   return (
     <Layout>
     	<Head>
     		<title>rénold</title>
-    	</Head>	
+    	</Head>
+		<ul>
+			{plants.map((plant) => (
+				<li>
+					<h2>{plant.name}</h2>
+					<p>{plant.watering}</p>
+				</li>
+			))}
+		</ul>
     </Layout>
   )
+}
+
+
+export async function getServerSideProps(){
+	var response = [];
+	await axios.get('http://localhost:3000/api/plants')
+	.then(function(answer){
+		response = answer.data
+		console.log(answer.data)
+	}).catch(function(error){
+		console.log(error);
+		//response = plants
+	}).then(function(){
+	})
+
+		return {
+			props : {
+				plants : response
+			}
+		}
+
 }
